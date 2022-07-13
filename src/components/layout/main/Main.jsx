8d54f94a2c9043af67fbfbcb6d70c13fb512c15b
Djengiz Ibrqmov <djengizibrqmov@gmail.com>
2022-07-13 14:41:57 +0300

    Implement Route protection

--- src/components/layout/main/Main.jsx
+++ src/components/layout/main/Main.jsx
@@ -1,12 +1,14 @@
-import {Routes, Route} from 'react-router-dom'
+import { Routes, Route } from "react-router-dom";
 import Login from '../../../pages/login/Login'
 import Home from '../../../pages/home/Home'
+import { AuthenticatedRoute } from '../../../utils/guards/AuthenticatedRoute'
+import { NonAuthenticatedRoute } from '../../../utils/guards/NonAuthenticatedRoute'
 
 export function Main(){
     return(
         <Routes>
-        <Route path="/login" element={<Login/>}/>
-        <Route path='/home' element={<Home/>}></Route>
+        <Route path="/login" element={<NonAuthenticatedRoute><Login /></NonAuthenticatedRoute>} />
+        <Route path='/home' element={<AuthenticatedRoute><Home/></AuthenticatedRoute>} />
         </Routes>
     )
 }

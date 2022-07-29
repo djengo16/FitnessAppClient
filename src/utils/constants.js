9c724bb3ab1d0715fef5b9a78da2684b2aef82ba
Djengiz Ibrqmov <djengizibrqmov@gmail.com>
2022-07-29 19:12:54 +0300

    Little message fix

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,7 @@
 export const DATA_PER_PAGE = 10;
 
 export const UNSAVED_CHANGES_MESSAGE =
-  "Do you want do close the window with unsaved data?";
+  "Do you want to close the window with unsaved data?";
 
 export const DELETE_EXERCISE_MESSAGE =
   "Are you sure you want to delete this exercise?";
